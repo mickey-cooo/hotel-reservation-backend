245442974a8b7ea032b36e3509d766c23c97a175
Phanupong-Phurahong <phanupong.pph@gmail.com>
2026-06-29 10:55:34 +0700

    refactor(stripe): 💡 use ConfigService for STRIPE_SECRET_KEY

--- src/stripe/stripe.provider.ts
+++ src/stripe/stripe.provider.ts
@@ -1,10 +1,13 @@
 import Stripe from 'stripe';
+import { ConfigService } from '@nestjs/config';
 
 export const StripeProvider = {
   provide: 'STRIPE_CLIENT',
-  useFactory: () => {
-    return new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  useFactory: (configService: ConfigService) => {
+    const secretKey = configService.get<string>('STRIPE_SECRET_KEY');
+    return new Stripe(secretKey!, {
       apiVersion: '2026-05-27.dahlia',
     });
   },
+  inject: [ConfigService],
 };
